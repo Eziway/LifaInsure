7a6262cf409091371cb91dd892f378a36b333dc0
BooiBotle <72595745+Eziway@users.noreply.github.com>
2026-08-21 01:54:57 +0200

    vite update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/LifaInsure/',
+  base: '/lifainsure/',
   plugins: [
     react(),
     tailwindcss(),
